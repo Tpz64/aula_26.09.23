067ac12db4498c5cbf53587a60e20011b36312c5
Thais <thaispz64@gmail.com>
2023-09-26 09:08:31 -0300

    Adcionando o for com array e of

--- script.js
+++ script.js
@@ -41,3 +41,16 @@ for (let i=0; i< 10; i++){
     console.log("Agora i vale" +i);
 }
 
+//for com array
+
+let carros=["fusca","belina","Marea Turbo","Kombi","Gurgel","passat"]
+
+for (let i=0; i< carros.length; i++){
+    console.log(carros[i])
+}
+
+// for com of
+
+for (let carro of carros){
+    console.log(carro)
+}
