1cd80d4b3cbadd7b954e9e53d5bafb0266433154
Thais <thaispz64@gmail.com>
2023-09-26 09:00:45 -0300

    Switch, incremento e decremento

---
+++ script.js
@@ -0,0 +1,36 @@
+// switch
+
+//let opcao = 4;
+
+/*
+let opcao = parseInt(prompt("Digite uma opção"));
+let opcao = +prompt("Digite uma opção")
+
+switch(opcao){
+    case 1:
+        document.write("Primeira Opção")
+        break;
+    case 2:
+        document.write("Segunda Opção")
+        break;
+    case 3: 
+        document.write("Terceira opção")
+        break;
+    default :
+        document.write("nehuma das opções validas")
+        break;
+
+}
+*/
+
+// Incremento e decremento
+
+let valor=7
+console.log(valor)
+valor = valor-1
+console.log(valor);
+
+let valor1=10
+console.log(valor1)
+valor1 +=3;
+console.log(valor1)
